179eac146d9eab6b45114e69e26d4ba6f6764c05
danielpdaniel <artofjquinn@gmail.com>
2023-07-23 12:53:12 -0400

    Update class with methods to get tests passing

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount=0, total=0):
+    self.discount = discount
+    self.total = total
+    self.items = []
+    self.transactions = []
+
+  def add_item(self, title='', price=0, quantity=1):
+    self.transactions.append({"title": title, "price": price, "quantity": quantity})
+    for i in range(0, quantity):
+      self.total += price
+      self.items.append(title)
+
+
+  def apply_discount(self):
+    discount_total = (self.discount / 100) * self.total
+    self.total = int(self.total - discount_total)
+    if self.discount == 0:
+      print("There is no discount to apply.")
+    else:
+      print(f"After the discount, the total comes to ${self.total}.")
+
+  def void_last_transaction(self):
+    transaction = self.transactions[len(self.transactions) - 1]
+    print(transaction)
+    for i in range(0, transaction["quantity"]):
+      self.total -= transaction["price"]
+
+    self.transactions.pop()
